002c70dea09d252fbd3e76726da34f20dc06e394
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-08 01:58:00 -0600

    load gauss data

--- gauss.py
+++ gauss.py
@@ -8,4 +8,9 @@ import scipy as sci
 import math as m
 import matplotlib as mplot
 
-
+def load_data():
+  filename = "data/motor.dat"
+  skip = 37
+  cols = (1,2)
+  data = np.loadtxt(filename, skiprows=skip, usecols=cols)
+  return data
