94a450b64fde7d7aaf3feb3dea80814902c95746
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-08 09:16:21 -0600

    build kernel matrix

--- gauss.py
+++ gauss.py
@@ -25,4 +25,22 @@ def plot_data(data):
 
   pl.show()
 
+class Gauss:
+  def __init__(self, sigma=0.5):
+    self.sig = sigma
+
+  def get_kernel_diff(self, x1, x2):
+    top = -1 * m.sqrt(x1**2 + x2**2)**2
+    return m.exp(top/(2*self.sig**2))
+
+  def build_kernel(self, data):
+    n = len(data)
+    self.K = np.reshape(np.zeros(n**2), (n,n))
+    for i in xrange(n):
+      for j in xrange(n):
+        self.K[i][j] = self.get_kernel_diff(data[i],data[j])
+
+
+
+
 plot_data(load_data())
