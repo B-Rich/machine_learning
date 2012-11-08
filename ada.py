fdf558b3dd3ba5f6dfb7830e07529fcfd46bad16
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-08 01:42:12 -0600

    added file stubs

---
+++ ada.py
@@ -0,0 +1,12 @@
+# ada.py
+# Bill Waldrep, November 2012
+#
+# Implementation of AdaBoost with decision stubs
+# as weak learners.
+
+import numpy as np
+import scipy as sci
+import math as m
+import matplotlib as mplot
+
+
---
+++ gauss.py
@@ -0,0 +1,11 @@
+# gauss.py
+# Bill Waldrep, November 2012
+#
+# Gaussian Process regression with a Gaussian kernel
+
+import numpy as np
+import scipy as sci
+import math as m
+import matplotlib as mplot
+
+
