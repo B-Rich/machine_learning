c5593bd7ed4a0d94a1eb754bed84e6c86a6bc1fa
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-08 02:29:17 -0600

    added loading and display functions for adaboost

--- ada.py
+++ ada.py
@@ -7,6 +7,21 @@
 import numpy as np
 import scipy as sci
 import math as m
-import matplotlib as mplot
+import matplotlib 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
+def load_data(suffix='train.csv'):
+  data_file = 'data/ada_x_' + suffix
+  label_file = 'data/ada_y_' + suffix
+  delim = ','
+  data = np.loadtxt(data_file, delimiter=delim)
+  labels = np.loadtxt(label_file, delimiter=delim)
+  return data, labels
 
+def show_image(img):
+  """Plot the input as a greyscale
+  image for sanity checking"""
+  temp = np.reshape(x[3], (28,28))
+  plt.imshow(temp, cmap = cm.Greys_r)
+  plt.show()
