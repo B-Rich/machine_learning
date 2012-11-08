5e280d5dcb20ece98baee909ffb9103d76d010b2
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-08 07:58:42 -0600

    plot the data

--- gauss.py
+++ gauss.py
@@ -6,11 +6,23 @@
 import numpy as np
 import scipy as sci
 import math as m
-import matplotlib as mplot
+import matplotlib
+from matplotlib import pyplot as pl
 
 def load_data():
   filename = "data/motor.dat"
   skip = 37
-  cols = (1,2)
+  cols = (0,1)
   data = np.loadtxt(filename, skiprows=skip, usecols=cols)
   return data
+
+def plot_data(data):
+  fig = pl.figure()
+  pl.plot(data[:,0], data[:,1], 'r:', label='foobar')
+  pl.xlabel('x')
+  pl.ylabel('y')
+  pl.legend(loc='upper left')
+
+  pl.show()
+
+plot_data(load_data())
