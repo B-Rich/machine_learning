1ba2af908673a61ff66cae915e89793e57b5f642
Bill Waldrep <bill.waldrep@gmail.com>
2012-11-27 10:15:57 -0600

    implemented generate and fixed compile errors

--- hmm.py
+++ hmm.py
@@ -31,6 +31,23 @@ def log_normalize_matrix(mat):
   """Normalize each row without underflow"""
   return np.apply_along_axis(log_normalize, 1, mat)
 
+def log_random():
+  """Take the log of a random number between 0 and 1"""
+  return np.log(random())
+
+def pick_item(p_vec):
+  """Pick a random item in a vector of log probabilities"""
+  r = random()
+  v = np.exp(p_vec)
+  val = v[0]
+  indx = 0
+  for i in range(1, len(p_vec)):
+    if val > r:
+      indx = i
+      break
+    val += v[i]
+  return indx
+
 def index_obs(obs_char):
   """Convert observed character to observed node index"""
   if obs_char == '*':
@@ -57,43 +74,59 @@ class HiddenMarkovModel:
     self.num_hidden = len(transitions)
     self.num_observed = 26
 
- def viterbi(self, emissions):
-   """Calculate most likely sequence of hidden states
+  def viterbi(self, emissions):
+    """Calculate most likely sequence of hidden states
       to result in 'ys'"""
-   obs_count = len(ys)
-   states = range(self.num_hidden):
+    obs_count = len(ys)
+    states = range(self.num_hidden)
 
-   # initialize DP table
-   table = np.zeros((self.num_hidden, obs_count))
-   path = np.zeros((self.num_hidden, obs_count))
+    # initialize DP table
+    table = np.zeros((self.num_hidden, obs_count))
+    path = np.zeros((self.num_hidden, obs_count))
 
-   # fill in first row
-   for s in states:
-     # add log values
-     table[s,0] = self.start[s] + self.obs[s][ys[0]]
-     path[s,0] = s
+    # fill in first row
+    for s in states:
+      # add log values
+      table[s,0] = self.start[s] + self.obs[s][ys[0]]
+      path[s,0] = s
 
-   # compute rest of table
-   for t in range(1,obs_count):
-     for s in states:
-       guess = -1 * np.inf
-       for ns in states:
-         # again, add log values
-         p = table[ns, t-1] + self.tran[ns,s] + self.obs[s][ys[t]]
-         if p < guess:
-           guess = p
-           path[s,t] = ns
-       table[s,t] = guess
+    # compute rest of table
+    for t in range(1,obs_count):
+      for s in states:
+        guess = -1 * np.inf
+        for ns in states:
+          # again, add log values
+          p = table[ns, t-1] + self.tran[ns,s] + self.obs[s][ys[t]]
+          if p < guess:
+            guess = p
+            path[s,t] = ns
+        table[s,t] = guess
 
-   # reconstruct path
-   fpath = []
-   for t in range(obs_count).reverse():
-     p = table[0,t]
-     g = 0
-     for s in states:
-       if table[s,t] > p:
-         p = table[s,t]
-         g = s
-     fpath.append(g)
+    # reconstruct path
+    fpath = []
+    for t in range(obs_count).reverse():
+      p = table[0,t]
+      g = 0
+      for s in states:
+        if table[s,t] > p:
+          p = table[s,t]
+          g = s
+      fpath.append(g)
 
-   return fpath.reverse()
+    return fpath.reverse()
+
+  def generate(self, length):
+    """Generate output with the HMM"""
+    out = []
+    states = range(self.num_hidden)
+    s = pick_item(self.start)
+    out.append(rand_pick(self.emissions[s,:]))
+ 
+    # do a random walk
+    while(length > len(out)):
+      s = pick_item(self.tran[s,:])
+      out.append(rand_pick(self.emissions[s,:]))
+ 
+    # translate and join the message
+    msg = ''.join(map(lambda c:show_obs(c),out))
+    return msg
