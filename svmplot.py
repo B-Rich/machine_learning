7750407601181967d68f187b3b289f8b2c9767be
Bill Waldrep <bill.waldrep@gmail.com>
2012-12-05 15:53:29 -0600

    added loading satimage files

--- svmplot.py
+++ svmplot.py
@@ -8,14 +8,55 @@
 # numeric stuff
 import numpy as np
 
+# plotting data
+from matplotlib import pyplot as pl
+
 # generate test data
 from sklearn.datasets.samples_generator import make_blobs
 
 # actual svm module
 import svm
 
-def make_2d_data(n_samples):
-  # generate two clusters of 
-  return make_blobs(n_samples=n_samples, centers=2)
+def make2dData(n_samples):
+  # generate a 2d 2-class dataset
+  X,y = make_blobs(n_samples=n_samples, centers=2)
+  y[y==0] = -1
+  return X,y
 
+def readSatData(fname='tr'):
+  data = []
+  with open('data/satimage.scale.' + fname, 'r') as f:
+    for line in f.readlines():
+      s = line.split()
+      row = np.zeros(37)
+      # set label
+      if s[0] == '6':
+        row[0] = 1
+      else:
+        row[0] = -1
+      hand = 1
+      for i in range(1,37):
+        k,v = s[hand].split(':')
+        if int(k) == i:
+          hand += 1
+          row[i] = float(v)
+        else:
+          row[i] = 0
+      data.append(row)
+  data = np.vstack(data)
+  y = data[:,0]
+  X = data[:,1:]
+  return X,y
 
+s = svm.SVM(10)
+#X,y = make2dData(200)
+#c.train(X,y)
+#print c.alphas
+#print "final error", c.test(X,y)
+#print c.findC(X,y,count=5)
+X,y = readSatData()
+c = s.findC(X,y,count=10,kfolds=3)
+s = svm.SVM(c)
+s.train(X,y)
+Xt,yt = readSatData('t')
+print "final error", s.test(Xt,yt)
