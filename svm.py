7d89eb58c0f85dc789bc45ca7e21fb78a3615071
Bill Waldrep <bill.waldrep@gmail.com>
2012-12-05 03:18:15 -0600

    fleshed out svm class

--- svm.py
+++ svm.py
@@ -7,9 +7,6 @@
 import numpy as np
 from numpy import linalg as lin
 
-# plotting data
-from matplotlib import pyplot as pl
-
 # SMO class for training
 from smo import SMO
 
@@ -32,14 +29,37 @@ class SVM:
   def __init__(self, kernel, C):
     self.k = kernel
     self.C = C
-    self.core = SMO(kernel, C)
+    self.optimizer = SMO(kernel, C)
 
   def train(self, X, y):
-    raise NotImplementedError
+    # store training examples
+    self.supv = X
+    self.supv_y = y
+
+    # use the SMO module to compute alphas and b
+    self.alphas = self.optimizer.compute_alphas(X,y)
+    self.b = self.optimizer.threshold
+
+  def _eval(self, x):
+    # evaluate the SVM on a single example
+    ret = 0
+    for i, a in enumerate(self.alphas):
+      # ignore non-support vectors
+      if a != 0:
+        ret += a * self.supv_y[i] * self.k.eval(x,self.supv[i])
+    return ret + self.b
+
+  def eval(self, X):
+    # evaluate a matrix of example vectors
+    return np.vectorize(self._eval)(X)
 
   def classify(self, X):
-    raise NotImplementedError
+    # classify a matrix of example vectors
+    return np.sign(self.eval(X))
 
   def test(self, X, y):
-    raise NotImplementedError
-
+    # find the percentage of misclassified examples
+    error = np.zeros(len(X))
+    guess = self.classify(X)
+    error[guess != y] = 1
+    return np.float(np.sum(error)) / len(X)
