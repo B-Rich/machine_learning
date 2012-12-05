9e54e2ab77aca24325cbef41411630d4d95811ea
Bill Waldrep <bill.waldrep@gmail.com>
2012-12-05 15:53:08 -0600

    finished svm

--- svm.py
+++ svm.py
@@ -26,7 +26,7 @@ class RBFKernel:
 
 class SVM:
   # Support Vector Machine Classifier
-  def __init__(self, kernel, C):
+  def __init__(self, C, kernel=LinKernel()):
     self.k = kernel
     self.C = C
     self.optimizer = SMO(kernel, C)
@@ -38,7 +38,7 @@ class SVM:
 
     # use the SMO module to compute alphas and b
     self.alphas = self.optimizer.compute_alphas(X,y)
-    self.b = self.optimizer.threshold
+    self.b = self.optimizer.b
 
   def _eval(self, x):
     # evaluate the SVM on a single example
@@ -51,7 +51,10 @@ class SVM:
 
   def eval(self, X):
     # evaluate a matrix of example vectors
-    return np.vectorize(self._eval)(X)
+    result = np.zeros(len(X))
+    for i in xrange(len(X)):
+      result[i] = self._eval(X[i])
+    return result
 
   def classify(self, X):
     # classify a matrix of example vectors
@@ -63,3 +66,36 @@ class SVM:
     guess = self.classify(X)
     error[guess != y] = 1
     return np.float(np.sum(error)) / len(X)
+
+  def findC(self, X, y, count=50, kfolds=5):
+    # find a good estimate of C with kfold cross validation
+
+    yt = y.reshape(len(y), 1)
+    data = np.hstack([yt,X])
+    np.random.shuffle(data)
+    partitions = np.array_split(data, kfolds)
+    candidates = np.logspace(0, 5, num=count, base=np.e)
+
+    minErr = np.inf
+    C = candidates[0]
+    for c in candidates:
+      errors = np.zeros(kfolds)
+      for i in range(kfolds):
+        test = partitions[i]
+        train = np.vstack([partitions[x] for x in range(kfolds) if x != i])
+        testy = test[:,0]
+        testx = test[:,1:]
+        trainy = train[:,0]
+        trainx = train[:,1:]
+        temp = SVM(c, self.k)
+        temp.train(trainx, trainy)
+        errors[i] = temp.test(testx, testy)
+      err = np.mean(errors)
+      print c, err, minErr
+      if err < minErr:
+        C = c
+        minErr = err
+
+    print candidates
+    print "Final value: ", C
+    return C
