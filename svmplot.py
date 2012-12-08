303e53f00334ebc3edf8b3e97ae78aeef1982796
Bill Waldrep <bill.waldrep@gmail.com>
2012-12-07 21:34:02 -0600

    added 2d decision boundary plot

--- svmplot.py
+++ svmplot.py
@@ -48,17 +48,45 @@ def readSatData(fname='tr'):
   X = data[:,1:]
   return X,y
 
-r = svm.RBFKernel(2)
+def plot2d(c, k, X, y):
+  xmin = np.min(X[:,0])
+  ymin = np.min(X[:,1])
+  xmax = np.max(X[:,0])
+  ymax = np.max(X[:,1])
+
+  density = 100
+  xx, yy = np.meshgrid(np.linspace(xmin, xmax, density),
+                       np.linspace(ymin, ymax, density))
+
+  s = svm.SVM(c, kernel=k)
+  s.train(X, y)
+  result = s.eval(np.c_[xx.ravel(), yy.ravel()])
+  result = result.reshape(xx.shape())
+  
+  pl.imshow(result, interpolation='nearest', extent=(xmin,xmax,ymin,ymax),
+            aspect='auto', origin='lower', cmap=pl.cm.Pu0r_r)
+  contours = pl.contour(xx,yy,result,levels=[0],linewidths=2,linetypes='--')
+  pl.scatter(X[:,0],X[:,1],s=30,c=y,cmap=pl.cm.Paired)
+  pl.show()
+
+
+sigma = 1.5
+r = svm.RBFKernel(sigma)
 s = svm.SVM(10,kernel=r)
 #X,y = make2dData(200)
 #c.train(X,y)
 #print c.alphas
 #print c.findC(X,y,count=5)
-X,y = readSatData()
-#c = s.findC(X,y,count=10,kfolds=3)
-c = 8.3
-s = svm.SVM(c,kernel=svm.RBFKernel(2))
-s.train(X,y)
-Xt,yt = readSatData('t')
-print "final error", s.test(Xt,yt)
+#X,y = readSatData()
+#c = s.findC(X,y,count=50,kfolds=5)
+#s = svm.SVM(c,kernel=svm.RBFKernel(sigma))
+#s.train(X,y)
+#Xt,yt = readSatData('t')
+#print "final error", s.test(Xt,yt)
 #print "final error", s.test(X,y)
+
+X,y = make2dData(20)
+s.train(X,y)
+print X
+print np.max(X[:,0]), np.min(X[:,0])
+print np.max(X[:,1]), np.min(X[:,1])
